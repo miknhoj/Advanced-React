5ad852b0f6471de28ecd9d860411b489fe6807f5
miknhoj <kimjohan86@gmail.com>
2018-12-20 13:58:36 -0500

    user signup and permissions flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -26,16 +29,39 @@ const Mutations = {
       info
     );
   },
-  async deleteItem(parent, args, ctx, info){
+  async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id }
     // 1. find the item
-    const item = await ctx.db.query.item({where}, `{id, title}`)
+    const item = await ctx.db.query.item({ where }, `{id, title}`)
     // 2. check if they own that item, or have the permissions
     // TODO
     // 3. delete it
-    return ctx.db.mutation.deleteItem({where}, info)
+    return ctx.db.mutation.deleteItem({ where }, info)
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10)
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ['USER'] }
+      }
 
-  }
+    }, info)
+    // create the JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    // we set the JWT as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    })
+    // finally we return the user to the browser
+    return user;
+  },
 };
 
 module.exports = Mutations;
